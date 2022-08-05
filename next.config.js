9bb268e7417c5dc3a86df1cd6a8cccc657bf5365
Sigurd Holten Bøen <sigholboe@gmail.com>
2022-08-06 01:02:52 +0200

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
 module.exports = {
   images: {
     loader: 'akamai',
     path: '/',
-  },
-  nextConfig
+  }
 }
